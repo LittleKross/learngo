3e52abd6ce0b78e170f3bedd08f17972a0bed739
LittleKross47 <btyoung2k@gmail.com>
2021-11-13 11:20:07 -0500

    Working on random item functions

--- webtest.go
+++ webtest.go
@@ -2,19 +2,21 @@ package main
 
 import(
 	"fmt"
+	"github.com/gorilla/mux"
 	"log"
 	"net/http"
 )
 
 func main() {
-	http.HandleFunc("/webpage",func(w http.ResponseWriter, r *http.Request) {
+	r := mux.NewRouter()
+	r.HandleFunc("/webpage",func(w http.ResponseWriter, r *http.Request) {
 		http.ServeFile(w,r,"home.html")
 		fmt.Fprintf(w,"Hello you've requsted: %s\n",r.URL.Path)
 		log.Printf("Hello you've requsted: %s\n",r.URL.Path)
 	})
-	http.HandleFunc("/api/v0/",func(w http.ResponseWriter, r *http.Request) {
-		log.Printf("Hello you've requsted: %s\n",r.URL.Path)
-		fmt.Fprintln(w,"You have requested: " + r.URL.Path)
+	r.HandleFunc("/api/v0/path/current",func(w http.ResponseWriter, r *http.Request) {
+		log.Printf("The current path is: %s\n",r.URL.Path)
+		fmt.Fprintln(w,"The current path is: " + r.URL.Path)
 	})
-	http.ListenAndServe(":80",nil)
+	http.ListenAndServe(":80",r)
 }
